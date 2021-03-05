23135a69d8da171b096cccf9c645017775a1ffb3
James Yeates <jimibue@gmail.com>
2021-03-05 10:27:00 -0700

    foo component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
+const Foo = () => {
+    return (
+        <div>
+            <h1>FOO</h1>
+        </div>
+    )
+}
+
 const App = () => {
     return (
         <> 
           <h1>App</h1>
+          <Foo />
         </>
     )
 }
